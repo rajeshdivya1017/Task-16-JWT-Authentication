6454814f7a7006f801c2355bb7957448104e87fe
rajeshdivya1017 <divyarajesh1007@gmail.com>
2026-07-08 14:39:00 +0530

    Add files via upload

---
+++ Navbar.jsx
@@ -0,0 +1,95 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+import { useAuth } from "../context/AuthContext";
+import api from "../api";
+import TokenCountdown from "./TokenCountdown";
+
+export default function Navbar() {
+  const { cartItems = [] } = useCart();
+  const { user, setUser } = useAuth();
+  const navigate = useNavigate();
+
+  const totalItems = cartItems.reduce(
+    (sum, item) => sum + (item.qty || 1),
+    0
+  );
+
+  const logout = async () => {
+  try {
+    await api.get("/api/logout");
+  } catch (err) {
+    console.log(err);
+  }
+
+  // Clear JWT data
+  localStorage.removeItem("access_token");
+  localStorage.removeItem("refresh_token");
+  localStorage.removeItem("user");
+
+  setUser(null);
+
+  navigate("/login");
+};
+
+  return (
+    <div className="navbar">
+
+      <h2>🛒 E-Commerce</h2>
+
+      <div className="nav-links">
+
+        <Link to="/">Home</Link>
+
+        {user?.role !== "admin" && (
+          <>
+            <Link to="/cart">
+              Cart ({totalItems})
+            </Link>
+
+            <Link to="/orders">
+              My Orders
+            </Link>
+          </>
+        )}
+
+        {user?.role === "admin" && (
+          <>
+            <Link to="/admin/products">
+              Products
+            </Link>
+
+            <Link to="/admin/orders">
+              Orders
+            </Link>
+          </>
+        )}
+
+        {!user ? (
+          <Link to="/login">
+            Login
+          </Link>
+        ) : (
+          <>
+            <span
+              style={{
+                fontWeight: "bold",
+                marginRight: "10px",
+              }}
+            >
+              👤 {user.name}
+            </span>
+              <TokenCountdown />
+            <button
+              className="logout-btn"
+              onClick={logout}
+            >
+              Logout
+            </button>
+          </>
+        )}
+
+      </div>
+
+    </div>
+  );
+}
